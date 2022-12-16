035af7615fb7dfb1fcbbfbba78e5bcafacd8a163
Sthefane <sthefanesoliveiraa@outlook.com>
2022-12-16 14:11:22 +0000

    database production

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,27 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
+     * On creation, give a new user a 30 random string for email verification
+     * Model event:
+     * triggered automatically
+     */
+    public static function boot ()
+    {
+        // trigger the boot method of the Model Class that Eloquent models extend
+        parent::boot();
+
+        // listen for model events
+        // When a user is created, add tokens
+        static::creating(function($user) {
+            $user->token = str_random(30); // composer require laravel/helpers
+        });
+
+        static::creating(function($user) {
+            $user->sub_token = str_random(30);
+        });
+    }
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
@@ -42,4 +63,18 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+//    /**
+//     * Get the registered user to confirm their email
+//     *
+//     * return boolean
+//     */
+//    public function confirmEmail ()
+//    {
+//        $this->verified = true;
+//        $this->token = null;
+//        $this->save();
+//
+//        return $this->verified;
+//    }
 }
