2124bac6322c3f25405d219acb9d47c8c852f9ae
Sthefane <sthefanesoliveiraa@outlook.com>
2023-08-07 18:57:52 +0100

    compile for production - hide logs

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,29 +2,21 @@ const mix = require('laravel-mix');
 
 require('laravel-mix-tailwind');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js').vue();
 
-// new webpack.ProvidePlugin({
-//     'window.Quill': 'quill/dist/quill.js',
-//     'Quill': 'quill/dist/quill.js'
-// })
 
-// mix.extend('foo',new class{
-//     webpackPlugins(){
-//         return new webpack.ProvidePlugin({
-//             "window.Quill": "quill/dist/quill.js",
-//             Quill: "quill/dist/quill.js"
-//         });
-//     }
-// });
+// remove comments when in production
+if (mix.inProduction())
+{
+    mix.options({
+        terser: {
+            terserOptions: {
+                compress: {
+                    drop_console: true
+                }
+            }
+        }
+    });
+
+    //mix.version();
+}
